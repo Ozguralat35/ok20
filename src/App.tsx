4ef39972f9329cca49f5c6b2b2058f4b6cd916f7
Ozguralat35 <ozguralat35@gmail.com>
2025-06-10 23:27:28 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -317,7 +317,7 @@ function App() {
         {/* Header */}
         <div className="w-full relative h-[10vh] min-h-[80px] max-h-[100px]">
           <img
-            src="/header.jpg"
+            src="/public/header.jpg"
             className="w-full h-full object-cover"
             alt="Header"
           />
